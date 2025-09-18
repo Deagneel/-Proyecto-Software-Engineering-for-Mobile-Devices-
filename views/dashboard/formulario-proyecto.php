f446d0e53508bd90e9425d13693b494ac746c4d6
Jesus R <79134963+Deagneel@users.noreply.github.com>
2025-09-18 01:40:36 -0600

    Add files via upload

---
+++ views/dashboard/formulario-proyecto.php
@@ -0,0 +1,18 @@
+<div class="campo">
+    <label for="proyecto">Nombre Proyecto: </label>
+    <input
+        type="text"
+        name="proyecto"
+        id="proyecto"
+        placeholder="Nombre del Proyecto"
+    />
+</div>
+<div class="campo">
+    <label for="proyecto">Fecha de entrega: </label>
+    <input
+        type="date"
+        name="fecha"
+        id="fecha"
+        placeholder="fecha"
+    />
+</div>
